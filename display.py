eabfe35384c6e9cdc6cdfca35455d2b8d20336e9
Vishnu <VishnuUnnikrishnan@users.noreply.github.com>
2019-04-07 17:39:00 +1000

    basic package manager working and settings also uploaded

--- display.py
+++ display.py
@@ -5,11 +5,14 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from settingsReader import readAttribute
-from modules.clockModule import clockThread
-
+from functools import partial
+import imp
+import logging
 import threading
 
 __moduleSettings__ = {}
+# Create the screen manager
+sm = ScreenManager()
 
 def getData():
     global __moduleSettings__
@@ -18,7 +21,7 @@ def getData():
 
 getData()
 
-# Declare both screens
+# Declare initial screens
 class MenuScreen(Screen):
     def __init__(self,**kwargs):
         super(MenuScreen, self).__init__(**kwargs)
@@ -29,20 +32,41 @@ class MenuScreen(Screen):
         layout = GridLayout(rows=rows, cols=cols)
         self.add_widget(layout)
         for i in range(0,rows*cols):
-            button = Button(text='Button '+str(i), font_size=20)
+            #set default button fields here, can be updated by user
+            button = Button(text='Button '+str(i), font_size=20, halign="center")
             self.connectModule(button,i)
             layout.add_widget(button)
 
     def connectModule(self, button, id):
-        if id == 0:
-            t = clockThread(button)
-            threading.Thread(target=t.clock).start()
+        global __moduleSettings__
+        try:
+            #Load Module
+            moduleName = __moduleSettings__["module_"+str(id)]
+            newModule = readAttribute("modules."+ moduleName)
+            module = imp.load_source(moduleName,"modules/"+moduleName+".py")
+            #Load class to be run
+            executeClass = getattr(module, newModule["class"])
+            
+            #Load Display for button click
+            displayClass = getattr(module, newModule["screen"])
+            sm.add_widget(displayClass(sm=sm, name = newModule["screen"]))
+
+            #Pass change screen from menu button to the context that was selected.
+            button.bind(on_press =partial(self.changeScreen, newModule["screen"]))
+            
+            #Run Module Code
+            t = executeClass(button)
+            threading.Thread(target=t.run).start()
+
+        except:
+            print("Error: No module_"+str(id)+" found")
         
+    def changeScreen(self, x, id):
+        print(type(x))
+        sm.current = x
 
-
-# Create the screen manager
-sm = ScreenManager()
-sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(MenuScreen(name="menu"))
+#sm.current = "menu"
 
 class rpiDisplay(App):
 
